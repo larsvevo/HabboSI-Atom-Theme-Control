1340802dd5947f7da15a682d81af6a2bb1008646
larsvevo <ardaadinc2003@gmail.com>
2024-11-25 21:03:02 +0300

    Update IndexController.php

--- src/Http/Controllers/IndexController.php
+++ src/Http/Controllers/IndexController.php
@@ -2,10 +2,11 @@
 
 namespace Atom\Theme\Http\Controllers;
 
-use Atom\Core\Models\CameraWeb;
+use Atom\Core\Models\WebsiteSetting;
 use Atom\Core\Models\WebsiteArticle;
 use Illuminate\Routing\Controller;
 use Illuminate\View\View;
+use Atom\Core\Models\User;
 
 class IndexController extends Controller
 {
@@ -20,12 +21,64 @@ class IndexController extends Controller
             ->limit(5)
             ->get();
 
-        $photos = CameraWeb::with('user')
-            ->latest('id')
-            ->where('approved', true)
-            ->limit(4)
+
+        $settings = WebsiteSetting::whereIn('key', ['min_staff_rank'])
+        ->pluck('value', 'key');
+
+
+        $credits = User::where('rank', '<', $settings->get('min_staff_rank'))
+            ->orderBy('credits', 'desc')
+            ->limit(1)
+            ->get();
+        
+
+        $duckets = User::with(['currencies' => fn ($query) => $query->where('type', 0)])
+            ->where('rank', '<', $settings->get('min_staff_rank'))
+            ->whereHas('currencies', fn ($query) => $query->where('type', 0))
+            ->join('users_currency', 'users.id', '=', 'users_currency.user_id')
+            ->where('users_currency.type', 0)
+            ->orderBy('users_currency.amount', 'desc')
+            ->select('users.*')
+            ->limit(1)
+            ->get();
+        
+
+        $diamonds = User::with(['currencies' => fn ($query) => $query->where('type', 5)])
+            ->where('rank', '<', $settings->get('min_staff_rank'))
+            ->whereHas('currencies', fn ($query) => $query->where('type', 5))
+            ->join('users_currency', 'users.id', '=', 'users_currency.user_id')
+            ->where('users_currency.type', 5)
+            ->orderBy('users_currency.amount', 'desc')
+            ->select('users.*')
+            ->limit(1)
             ->get();
 
-        return view('index', compact('articles', 'photos'));
+        $onlineTimes = User::with('settings')
+            ->where('rank', '<', $settings->get('min_staff_rank'))
+            ->join('users_settings', 'users.id', '=', 'users_settings.user_id')
+            ->orderBy('users_settings.online_time', 'desc')
+            ->select('users.*')
+            ->limit(1)
+            ->get();
+
+        $respects = User::with('settings')
+            ->where('rank', '<', $settings->get('min_staff_rank'))
+            ->join('users_settings', 'users.id', '=', 'users_settings.user_id')
+            ->orderBy('users_settings.respects_received', 'desc')
+            ->select('users.*')
+            ->limit(1)
+            ->get();
+
+        
+        $achievements = User::with('settings')
+            ->where('rank', '<', $settings->get('min_staff_rank'))
+            ->join('users_settings', 'users.id', '=', 'users_settings.user_id')
+            ->orderBy('users_settings.achievement_score', 'desc')
+            ->select('users.*')
+            ->limit(11)
+            ->get();
+
+        
+        return view('index', compact('articles','credits','duckets','diamonds','onlineTimes','respects','achievements',));
     }
 }
